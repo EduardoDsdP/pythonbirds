453c96dff90db0110759922791bbdfc69b5fc56a
Eduardodsdp <eduardodsdp@gmail.com>
2023-04-05 13:04:46 -0300

    Criada a classe Pessoa. Criado atributos de instancia. Criado atributo complexo filhos.

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,18 +1,23 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35, altura=1.90):   #essa linha se trata do comando de atributo de inicialização
+    def __init__(self, *filhos, nome=None, idade=35, altura=1.90):   #essa linha se trata do comando de atributo de inicialização
         self.altura = altura
         self.idade = idade
         self.nome = nome   # e aqui uso o none para indicar que terei um campo sem valor atribuido a ele ainda.
+        self.filhos = list(filhos)
+
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
 
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    p.nome = 'Eduardo'
-    print(p.nome)
-    print(p.idade)
-    print(p.altura)
+
+    eliza = Pessoa(nome='Eliza')
+    Eduardo = Pessoa(eliza, nome='Eduardo')
+    print(Pessoa.cumprimentar(Eduardo))
+    print(id(Eduardo))
+    print(Eduardo.cumprimentar())
+    print(Eduardo.idade)
+    print(Eduardo.altura)
+    for filho in Eduardo.filhos:
+        print(filho.nome)
